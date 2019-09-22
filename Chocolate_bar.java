0d14b54b9a6c4974af8999e1c58cb8f52da0839c
sanjay-3129 <49227865+sanjay-3129@users.noreply.github.com>
2019-09-22 17:12:29 +0530

    Add files via upload

---
+++ Chocolate_bar.java
@@ -0,0 +1,25 @@
+//birthday chocolate bar
+import java.util.Scanner;
+public class Chocolate_bar{
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int arr[] = new int[n];
+		for(int i=0; i<n; i++){
+			arr[i] = sc.nextInt();
+		}
+		int d = sc.nextInt();
+		int m = sc.nextInt();
+		int ways = 0;
+		for(int i=0; i<=n-m; i++){
+			int count = 0;
+			for(int j=i; j<i+m; j++){
+				count = count + arr[j];
+			}
+			if(count==d){
+				ways++;
+			}
+		}
+		System.out.println(ways);
+	}
+}
